c3c41500e1dd16c0232dee2f3ee83fb6c31d8119
shreyanslunkad68 <shreyanslunkad68@gmail.com>
2026-07-11 23:37:54 +0530

    Time: 0 ms (100%), Space: 23 MB (52.23%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int sum = 0;
+        vector<int> candies(n,1);
+        for(int i =1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        
+        for(int i =n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                candies[i]=max(candies[i], 1+candies[i+1]);
+            }
+        }
+
+        
+        for(int i = 0;i<candies.size();i++)
+        {
+            sum+=candies[i];
+        }   
+
+        return sum;
+    }
+};
