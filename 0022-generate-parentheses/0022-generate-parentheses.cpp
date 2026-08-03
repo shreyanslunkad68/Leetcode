e65f9ba42114ddd577b28cf1840910bfc1f3a9de
shreyanslunkad68 <shreyanslunkad68@gmail.com>
2026-08-03 22:20:12 +0530

    Time: 3 ms (69.32%), Space: 15.9 MB (36.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<string> ans;
-    void f(int left, int right, int n, string s)
-    {
-        if(right == n && left == n)
-        {
-            ans.push_back(s);
+    void f(int left, int right, int n, string temp){
+        if(2*n==temp.size()){
+            ans.push_back(temp);
+            return;
         }
 
-        if(left<n)
-        {
-        f(left+1, right, n, s+"(");
+        if(left<n){
+            f(left+1, right, n, temp+'(');
         }
-        if(right<left)
-        {
-        f(left, right+1, n, s+")");
+        if(right<left){
+            f(left, right+1, n, temp+')');
         }
-
     }
     vector<string> generateParenthesis(int n) {
-        string s ="";
-        f(0, 0, n, s);
+        f(0, 0, n, "");
         return ans;
     }
 };
